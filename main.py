25753d5e02b12dbb03d7e5ef0c1ae9e83e09ff18
Anigma Creative <anigmacreative@Anigmas-Mac-mini.local>
2025-09-10 21:07:37 -0400

    update function name in main import for count_words

--- main.py
+++ main.py
@@ -14,24 +14,6 @@ def main():
 
 if __name__ == "__main__":
     main()  
-      
+
 #count words
-from pathlib import Path
-
-def get_book_text(filepath: str) -> str:
-    with open(filepath, "r", encoding="utf-8") as f:
-        return f.read()
-
-def count_words(text: str) -> int:
-    # Split on any whitespace and count
-    return len(text.split())
-
-def main():
-    base = Path(__file__).resolve().parent
-    path = base / "books" / "frankenstein.txt"
-    text = get_book_text(path)
-    num_words = count_words(text)
-    print(f"{num_words} words found in the document")
-
-if __name__ == "__main__":
-    main()
+from stats import count_words
